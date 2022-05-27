4819aa3d6a154352e09c99bdc28421751631e2f7
Phani Ravikanth Mangipudi <phani.ravikanthmangipudi@smartsheet.com>
2022-05-27 11:47:55 -0700

    random string

--- local.tf
+++ local.tf
@@ -1,5 +1,9 @@
 resource "local_file" "testLocalFile" {
   filename = "ravi01.txt"
   content  = "foo!"
-  file_permission = 700
+  file_permission = 0700
+}
+
+resource "random_string" "testRandomString" {
+  length = 10
 }
