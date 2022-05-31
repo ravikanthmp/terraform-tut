78eb2971cebcc72864f0671726072259ea54aaa1
ravi <ravikanthmp123@gmail.com>
2022-05-31 14:58:15 -0700

    variable types

--- vars.tf
+++ vars.tf
@@ -1,3 +1,43 @@
 variable "filename" {
   default = "ravikanyeTest.txt"
 }
+
+variable "testNumber"{
+    type = number
+    default = 13
+}
+
+variable "testString"{
+    type = string
+    default = 13
+}
+
+variable "testList"{
+    type = list
+    default = ["mpravika", 2, "hahahaha"]
+}
+
+variable "testTuple"{
+    type = tuple([number, string])
+    default = ["12", "hahahaha"]
+}
+variable "testObject"{
+    type = object({
+        name =  string
+        id = number
+        favFood = list(string)
+    })
+    default = {
+        name = "ravi"
+        id = 21
+        favFood = ["aalo"]
+    }
+}
+
+variable "testMap"{
+    type = map
+    default = {
+        "name" = "mpravika"
+        "id" = 123
+    }
+}
